727e5d9fdf06df23018e9f85219d7f4d6316057f
JelindoGames <51799620+JelindoGames@users.noreply.github.com>
2021-01-29 13:35:49 -0500

    Basic Player Movement
    Basic camera (mouse) / movement (WASD) player controls. Player should be able to climb slopes and stairs of a certain height.

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] float mouseSensitivity = 100f;
+    [SerializeField] Transform playerMain = null;
+    float vertRot = 0f;
+
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // Horizontal Mouse Controls
+        playerMain.transform.Rotate(Vector3.up * mouseX);
+
+        // Vertical Mouse Controls
+        vertRot -= mouseY;
+        vertRot = Mathf.Clamp(vertRot, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(vertRot, 0, 0);
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] CharacterController controller = null;
+    [SerializeField] float speed = 5f;
+
+    [SerializeField] float gravityForce = -9.81f;
+    float gravityVel = 0;
+    [SerializeField] Transform bottom = null;
+    [SerializeField] LayerMask noPlayer;
+
+    void Update()
+    {
+        // Horizontal Movement
+
+        float x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        Vector3 move = (transform.right * x) + (transform.forward * z);
+        controller.Move(move);
+
+        controller.Move(transform.up * gravityVel * Time.deltaTime);
+    }
+
+    void FixedUpdate()
+    {
+        // Falling physics
+
+        if (Physics.CheckSphere(bottom.position, 0.25f, noPlayer))
+        {
+            gravityVel = 0;
+        }
+        else
+        {
+            gravityVel += gravityForce * Time.deltaTime;
+        }
+    }
+}
