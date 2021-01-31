bb571a672a2ec33462806b298d525313107df50e
cmrnprry <perry.ca@husky.neu.edu>
2021-01-31 15:36:09 -0500

    credits and title

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Debug.Log("Start");
+        SceneManager.LoadScene("Main_Scene");
+    }
+
+    public void Credits()
+    {
+        Debug.Log("Credits");
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void MainMenu()
+    {
+        Debug.Log("Main");
+        SceneManager.LoadScene("TitleScene");
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
