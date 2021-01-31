ab9bf14a3a6d89979095acc9db95f1957b518111
fortmax <fortna.m@husky.neu.edu>
2021-01-31 12:23:10 -0500

    Add Hot Topic

--- Assets/Scripts/TeleportTo.cs
+++ Assets/Scripts/TeleportTo.cs
@@ -7,11 +7,14 @@ public class TeleportTo : MonoBehaviour
 {
     public Transform TeleportPoint;
 
+    public float PlayerSpeed = 5f;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
             CharacterController control = other.GetComponent<CharacterController>();
+            other.GetComponent<PlayerMovement>().speed = PlayerSpeed;
             control.enabled = false;
             Vector3 rot = TeleportPoint.rotation.eulerAngles;
             other.transform.SetPositionAndRotation(TeleportPoint.position, Quaternion.Euler(0, rot.y, 0));
@@ -19,6 +22,7 @@ public class TeleportTo : MonoBehaviour
             look.enabled = false;
             look.vertRot = rot.x;
             look.transform.rotation = Quaternion.Euler(rot.x, 0, 0);
+            other.transform.localScale = TeleportPoint.localScale;
             DOTween.Sequence().AppendInterval(0.01f).AppendCallback(() =>
             {
                 control.enabled = true;
