7e4c7eceb9722c6dc4da71866c5bb028bb716ee5
cmrnprry <perry.ca@husky.neu.edu>
2021-01-30 21:15:50 -0500

    fish room and a lot of others are made

---
+++ Assets/Scripts/FishEnding.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class FishEnding : MonoBehaviour
+{
+    bool inFishRoom = false;
+    public GameObject fish;
+    public Transform target;
+    public float step;
+
+    public TextMeshProUGUI text;
+    private int count = 0;
+
+    private string words = "You've been here awile, better wake up before you forget how :)";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !inFishRoom)
+        {
+            inFishRoom = true;
+            StartCoroutine(FishEndingTime());
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && !inFishRoom)
+        {
+            inFishRoom = true;
+            StartCoroutine(FishEndingTime());
+        }
+    }
+
+
+
+    IEnumerator FishEndingTime()
+    {
+        while(fish.transform.position != target.position)
+        {
+            fish.transform.position = Vector3.MoveTowards(fish.transform.position, target.position, step);
+        }
+
+        StartCoroutine(Text());
+
+        
+        yield return null;
+    }
+
+    IEnumerator Text()
+    {
+
+        text.text += words[count];
+        count += 1;
+
+        if (count > words.Length)
+        {
+            SceneManager.LoadScene("Credits");
+            yield return null; }
+        else
+        {
+            yield return new WaitForSecondsRealtime(.25f);
+            StartCoroutine(Text());
+        }
+    }
+}
